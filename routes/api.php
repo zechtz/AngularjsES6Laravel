94abe56563f9fa15b2e3ab84a4bd464fd28ccee3
Zachariah Ngonyani <zachariahn@gmail.com>
2018-01-27 19:04:00 +0300

    Reformat api routes

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,10 @@ Route::get('/', function () {
 });
 
 Route::group(["prefix" => "v1"], function(){
-  Route::get('institutions'           , 'Setup\InstitutionsController@index');
-  Route::post('institutions'          , 'Setup\InstitutionsController@create');
-  Route::get('institutions/{id}/edit' , 'Setup\InstitutionsController@edit');
-  Route::put('institutions/{id}'      , 'Setup\InstitutionsController@update');
-  Route::delete('institutions/{id}'   , 'Setup\InstitutionsController@delete');
+    Route::get('institutions'           , 'Setup\InstitutionsController@index');
+    Route::post('institutions'          , 'Setup\InstitutionsController@create');
+    Route::get('institutions/{id}'      , 'Setup\InstitutionsController@show');
+    Route::get('institutions/{id}/edit' , 'Setup\InstitutionsController@edit');
+    Route::put('institutions/{id}'      , 'Setup\InstitutionsController@update');
+    Route::delete('institutions/{id}'   , 'Setup\InstitutionsController@destroy');
 });
