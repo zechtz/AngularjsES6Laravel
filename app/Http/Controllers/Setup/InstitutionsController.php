b51fda66465c9f95854e22e5551b10522d8a668b
Milton Isaya <miltonisaiah@gmail.com>
2018-01-27 15:02:06 +0300

    Institutions controller methods for crud have been added

--- app/Http/Controllers/Setup/InstitutionsController.php
+++ app/Http/Controllers/Setup/InstitutionsController.php
@@ -11,4 +11,40 @@ class InstitutionsController extends Controller
         $institution = Institution::all();
         return customApiResponse($institution, 'Success');
     }
+
+    public function create(Request $request){
+        $data = json_decode($request->getContent());
+        $institution = new Institution();
+        $institution->name = $data->name;
+        $institution->institution_id = $data->institution_id;
+        $institution->address = $data->address;
+        $institution->email = $data->email;
+        $institution->phone_number = $data->phone_number;
+        $institution->additional_information =$data->additional_information;
+        $institution->save();
+        $institutions = Institution::all();
+        return customApiResponse($institutions::all(),'Sucess');
+    }
+
+    public function update(Request $request){
+        $data = json_decode($request->getContent());
+        $institution = Institution::find($data->id);
+        $institution->name = $data->name;
+        $institution->institution_id = $data->institution_id;
+        $institution->address = $data->address;
+        $institution->email = $data->email;
+        $institution->phone_number = $data->phone_number;
+        $institution->additional_information =$data->additional_information;
+        $institution->save();
+        $institutions = Institution::all();
+        return customApiResponse($institutions::all(),'Sucess');
+    }
+
+    public function delete(Request $request){
+        $data = json_decode($request->getContent());
+        $institution = Institution::find($data->id);
+        $institution->delete();
+        $institutions = Institution::all();
+        return customApiResponse($institutions::all(),'Sucess');
+    }
 }
