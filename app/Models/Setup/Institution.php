b9783362ce3f8b7c32ad234f0a7ef59f8b82d29f
Zachariah Ngonyani <zachariahn@gmail.com>
2018-01-27 19:04:43 +0300

    Add validation rules in Institution model

--- app/Models/Setup/Institution.php
+++ app/Models/Setup/Institution.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Institution extends Model
 {
-    //
+    public static $rules = [
+        "name"  => "required|unique",
+        "email" => "required|unique",
+    ];
+
+    public static $create_rules = [
+        "name"  => "required|unique",
+        "email" => "required|unique:institutions",
+    ];
 }
