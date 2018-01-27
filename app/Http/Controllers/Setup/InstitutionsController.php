91db5e7f23fa47ee2c3923377cb68e2e42e0a804
Zachariah Ngonyani <zachariahn@gmail.com>
2018-01-27 19:05:09 +0300

    Apply validation rules in Institution controller

--- app/Http/Controllers/Setup/InstitutionsController.php
+++ app/Http/Controllers/Setup/InstitutionsController.php
@@ -4,58 +4,84 @@ namespace App\Http\Controllers\Setup;
 use App\Models\Setup\Institution;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class InstitutionsController extends Controller
 {
-    public function index (){
-        $institutions = Institution::all();
-        $institutions = customPaginate($institutions, 10);
+    public function index (Request $request){
+        $data         =  $request->all();
+        $per_page     =  isset($data['per_page'])? $data['per_page'] : 2;
+        $institutions =  Institution::all();
+        $institutions =  customPaginate($institutions, $per_page);
         return customApiResponse($institutions);
     }
 
     public function create(Request $request){
-        $data = json_decode($request->getContent());
-        $institution = new Institution();
-        $institution->name = $data->name;
-        $institution->institution_id = $data->institution_id;
-        $institution->address = $data->address;
-        $institution->email = $data->email;
-        $institution->phone_number = $data->phone_number;
-        $institution->additional_information = $data->additional_information;
-        $institution->save();
-        $institutions = Institution::all();
-        $institutions = customPaginate($institutions, 10);
-        return customApiResponse($institutions,'SUCCESSFULLY_CREATED');
-    }
-    public function update(Request $request){
-        $data = json_decode($request->getContent());
-        $institution = Institution::find($data->id);
-        $institution->name = $data->name;
-        $institution->institution_id = $data->institution_id;
-        $institution->address = $data->address;
-        $institution->email = $data->email;
-        $institution->phone_number = $data->phone_number;
-        $institution->additional_information = $data->additional_information;
-        $institution->save();
-        $institutions = Institution::all();
-        $institutions = customPaginate($institutions, 10);
-        return customApiResponse($institutions,'SUCCESSFULLY_UPDATED');
+        $data      = $request->all();
+        $validator = Validator::make($data, Institution::$create_rules);
+
+        if ($validator->fails()) {
+            return customApiResponse($data, "Validation Error", 400, $validator->errors()->all());
+        }
+
+        $result = Institution::create($data);
+
+        if($result) {
+            return customApiResponse($result, 'SUCCESSFULLY_CREATED', 201);
+        } else {
+            return customApiResponse($data, 'ERROR', 500);
+        }
     }
 
-    public function delete(Request $request){
-        $data = json_decode($request->getContent());
-        $institution = Institution::find($data->id);
-        $institution->delete();
-        $institutions = Institution::all();
-        $institutions = customPaginate($institutions, 10);
-        return customApiResponse($institutions,'SUCCESSFULLY_DELETED');
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $institution = Institution::find($id);
+        if ($institution != null) {
+            return customApiResponse($institution, 'SUCCESSFULL');
+        } else {
+            return customApiResponse('', 'Institution Not Found', 404, ['Institution Not Found']);
+        }
     }
-    public function remove(Request $request){
-        $data = json_decode($request->getContent());
-        $institution = Institution::find($data->id);
-        $institution->delete();
-        $institutions = Institution::all();
-        $institutions = customPaginate($institutions, 10);
-        return customApiResponse($institutions,'SUCCESSFULLY_DELETED');
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data      = $request->all();
+        $validator = Validator::make($data, Institution::$rules);
+
+        if ($validator->fails()) {
+            return customApiResponse($data, 'Validation error', 400, $validator->errors()->all());
+        }
+
+        $institution =  Institution::find($id);
+        $result      =  $institution->update($data);
+
+        if ($result) {
+            return customApiResponse($result, 'SUCCESSFULLY_UPDATED', 200);
+        } else {
+            return customApiResponse(' ', 'Error updating Institution ', 500);
+        }
+    }
+
+    public function destroy(Request $request){
+        $data        =  $request->all();
+        $institution =  Institution::find($data->id);
+        if ($institution->delete()) {
+            return customApiResponse($institution, 'SUCCESSFULLY_DELETED', 200);
+        } else {
+            return customApiResponse('', 'Error Deleting Institution ');
+        }
     }
 }
