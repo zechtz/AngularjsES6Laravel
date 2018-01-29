591df890fb00306aab60ff83bc52ce260247c2d9
Milton Isaya <miltonisaiah@gmail.com>
2018-01-29 18:49:29 +0300

    sp code and nullables have been added to the institutions migration

--- database/migrations/2018_01_27_104345_create_institutions_table.php
+++ database/migrations/2018_01_27_104345_create_institutions_table.php
@@ -16,11 +16,12 @@ class CreateInstitutionsTable extends Migration
         Schema::create('institutions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('sp_code')->nullable();
             $table->integer('institution_id')->unsigned()->nullable();
-            $table->text('address');
-            $table->string('email')->unique();
-            $table->string('phone_number');
-            $table->text('additional_information');
+            $table->text('address')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->nullable();
+            $table->text('additional_information')->nullable();
             $table->timestamps();
             $table->foreign('institution_id')
                 ->references('id')
