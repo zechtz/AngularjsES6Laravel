00f27229ed0f8114c021c85033bfc391ce73c6d9
BAKARI SAIDI SHEGHEMBE <sheghembebakari@gmail.com>
2018-01-30 19:18:05 +0300

    Created reserve hierarchies controller

---
+++ app/Http/Controllers/Setup/ReserveHierarchiesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Setup;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReserveHierarchiesController extends Controller
+{
+    //
+}
