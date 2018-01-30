a2c89b442933b06fe4af216d1f6f413b1b51906e
BAKARI SAIDI SHEGHEMBE <sheghembebakari@gmail.com>
2018-01-30 15:17:20 +0300

    Added relations in the stations table

--- database/migrations/2018_01_30_071357_create_stations_table.php
+++ database/migrations/2018_01_30_071357_create_stations_table.php
@@ -16,14 +16,14 @@ class CreateStationsTable extends Migration
         Schema::create('stations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('station_id')->unsigned()->nullable();
-            $table->integer('location_id')->unsigned();
-            $table->integer('institution_id')->unsigned();
-            $table->integer('station_category_id')->unsigned();
             $table->text('description');
             $table->float('latitude');
             $table->float('longitude');
             $table->string('shape_file_path');
+            $table->integer('station_id')->unsigned()->nullable();
+            $table->integer('location_id')->unsigned();
+            $table->integer('institution_id')->unsigned();
+            $table->integer('station_category_id')->unsigned();
             $table->foreign('station_id')
                 ->references('id')
                 ->on('stations')
