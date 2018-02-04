bcee015a3b8c4eb379d8c62024b64267bfbadfd3
Zachariah Ngonyani <zachariahn@gmail.com>
2018-02-04 15:52:53 +0300

    Add main controller

---
+++ public/js/MainController.js
@@ -0,0 +1,38 @@
+'use strict';
+
+export default class MasterController {
+  constructor($mdSidenav, $mdMedia) {
+      this.$mdSidenav  =  $mdSidenav;
+      this.$mdMedia    =  $mdMedia;
+      this.leftSidebar =  false;
+  }
+
+  swipeLeft() {
+    this.$mdSidenav('left').toggle().then(function(){
+    });
+  }
+
+  showHamburger() {
+    return (!this.leftSidebar || !this.$mdMedia('gt-md'));
+  }
+
+  showLeft() {
+    this.leftSidebar = true;
+    this.$mdSidenav('left').open().then(function() {
+    });
+  }
+
+  closeLeft() {
+    this.$mdSidenav('left').close().then(function() {
+      this.leftSidebar = false;
+    });
+  }
+
+  toggleSidenav(menuId) {
+    this.$mdSidenav(menuId).toggle();
+  }
+}
+
+MasterController.$inject = ['$mdSidenav', '$mdMedia'];
+
+
