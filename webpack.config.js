d1cc9948f6b3e4d2dd8b575f5d415a6620ac9870
Zachariah Ngonyani <zachariahn@gmail.com>
2018-11-23 10:35:26 +0300

    update node packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const UglifyJsPlugin =  require('uglifyjs-webpack-plugin');
+const ProvidePlugin  =  require('webpack-provide-global-plugin');
+const Dotenv         =  require('dotenv-webpack');
 
 const config =  {
   watch   : true,
@@ -16,11 +18,32 @@ const config =  {
       {test : /\.html$/, loader: 'raw-loader', exclude : '/node_modules/'},
       {test : /\.css$/, loader: 'style-loader!css-loader', exclude : '/node_modules/'},
       {test : /\.(scss|sass)$/, loader: 'style-loader!css-loader!sass-loader', exclude : '/node_modules/'},
-      {test : /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file-loader?name=public/fonts/[hash]-[name].[ext]' },
-      {test : /\.(png|jp(e*)g|svg)$/, loader: 'url-loader?name=public/images/[hash]-[name].[ext]'},
+      {test : /\.(eot|ttf|woff|woff2)$/, loader: 'file-loader?name=public/fonts/[hash]-[name].[ext]' },
+      /* {test : /\.svg/, exclude: ['/images/'],  loader: 'file-loader?name=public/fonts/[hash]-[name].[ext]'}, */
+      {test : /\.(png|jp(e*)g|svg|gif)$/, loader: 'url-loader?name=public/images/[hash]-[name].[ext]'},
+      {test : require.resolve('jquery'), use: [{ loader: 'expose-loader', options: 'jQuery' },{ loader: 'expose-loader', options: '$' }]},
+      {test : require.resolve('moment'), use: [{ loader: 'expose-loader', options: 'moment' },{ loader: 'expose-loader', options: 'moment' }]}
     ]
   },
   plugins : []
 };
+
+config.plugins.push(
+  new ProvidePlugin({
+    'window.jQuery'  : 'jquery',
+    jQuery           : 'jquery',
+    $                : 'jquery',
+    moment           : 'moment',
+    humanizeDuration : 'humanize-duration'
+  }),
+  new Dotenv()
+);
+
+/**
+config.plugins.push(
+  new UglifyJsPlugin({
+    test: /\.js($|\?)/i
+  })
+); */
 
 module.exports = config;
