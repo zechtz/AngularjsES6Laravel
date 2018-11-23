ca8521eb6258b847fd4aee811b5500c3b19f0ede
Zachariah Ngonyani <zachariahn@gmail.com>
2018-11-23 10:34:38 +0300

    Add production and development webpack configuration scripts

---
+++ dev.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const webpack        =  require('webpack');
+const merge          =  require('webpack-merge');
+const UglifyJsPlugin =  require('uglifyjs-webpack-plugin');
+const baseConfig     =  require('./base.config.js');
+const ProvidePlugin  =  require('webpack-provide-global-plugin');
+const Dotenv         =  require('dotenv-webpack');
+
+const config = merge(baseConfig, {
+  watch   : true,
+  devtool: 'eval-source-map',
+  context : __dirname + '/public/js',
+  entry   : __dirname + '/public/js/app.module.js',
+  output : {
+    path     : __dirname + '/public/src/',
+    filename : 'app.bundle.js'
+  },
+
+  module : {
+    rules: [
+      {test : require.resolve('jquery'), use: [{ loader: 'expose-loader', options: 'jQuery' },{ loader: 'expose-loader', options: '$' }]},
+      {test : require.resolve('moment'), use: [{ loader: 'expose-loader', options: 'moment' },{ loader: 'expose-loader', options: 'moment' }]}
+    ]
+  }
+});
+
+config.plugins.push(
+  new ProvidePlugin({
+    'window.jQuery'  : 'jquery',
+    jQuery           : 'jquery',
+    $                : 'jquery',
+    moment           : 'moment',
+    humanizeDuration : 'humanize-duration'
+  }),
+  new Dotenv()
+);
+
+module.exports = config;
